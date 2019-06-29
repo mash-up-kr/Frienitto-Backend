5e43aafc5b5e2b5ba8453e7e4d4c520400dd9f5a
gksxodnd007 <gksxodnd007@gmail.com>
2019-06-30 00:48:06 +0900

    [Feature] 회원 가입 및 로그인 기능 개발

---
+++ manitto/src/main/kotlin/org/frienitto/manitto/service/UserService.kt
@@ -0,0 +1,33 @@
+package org.frienitto.manitto.service
+
+import org.frienitto.manitto.domain.User
+import org.frienitto.manitto.dto.AccessToken
+import org.frienitto.manitto.dto.SignInDto
+import org.frienitto.manitto.dto.SignUpDto
+import org.frienitto.manitto.dto.UserDto
+import org.frienitto.manitto.exception.NonAuthorizationException
+import org.frienitto.manitto.repository.UserRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserService(private val userRepository: UserRepository) {
+
+    @Transactional
+    fun signUp(signUpDto: SignUpDto): UserDto {
+        val user = User.newUser(username = signUpDto.username, description = signUpDto.description, imageCode = signUpDto.imageCode, email = signUpDto.email, password = signUpDto.password)
+        userRepository.save(user)
+
+        return UserDto.from(user)
+    }
+
+    fun signIn(signInDto: SignInDto): AccessToken {
+        val user = userRepository.findByEmail(signInDto.email) ?: throw NonAuthorizationException()
+
+        if (user.password != signInDto.password) {
+            throw NonAuthorizationException()
+        }
+
+        return AccessToken(user.token, user.tokenExpiresDate)
+    }
+}
--- manitto/src/main/kotlin/org/frienitto/manitto/util/TokenUtils.kt
+++ manitto/src/main/kotlin/org/frienitto/manitto/util/TokenUtils.kt
@@ -3,9 +3,9 @@ package org.frienitto.manitto.util
 import java.net.InetAddress
 import java.security.MessageDigest
 
-fun generateToken(key: String, length: Int = 64): String {
+fun generateToken(key: String = "", length: Int = 64): String {
     return MessageDigest.getInstance("SHA-256")
-            .digest((System.nanoTime().toString() + InetAddress.getLocalHost().hostAddress + key).toByteArray())
+            .digest((System.currentTimeMillis().toString() + InetAddress.getLocalHost().hostAddress + key).toByteArray())
             .joinToString("") { "%02X".format(it) }
             .take(length)
 }
