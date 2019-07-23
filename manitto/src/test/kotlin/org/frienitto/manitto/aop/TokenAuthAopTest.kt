0d64af55f5714056e934612c69eb21a9e73ac21a
gksxodnd007 <gksxodnd007@gmail.com>
2019-07-23 21:40:05 +0900

    [Feature] aop 단위 테스트로 적합하지않아 테스트 수정

--- manitto/src/test/kotlin/org/frienitto/manitto/aop/TokenAuthAopTest.kt
+++ manitto/src/test/kotlin/org/frienitto/manitto/aop/TokenAuthAopTest.kt
@@ -1,17 +1,23 @@
 package org.frienitto.manitto.aop
 
-import org.frienitto.manitto.SpringWebMvcTestSupport
+import org.frienitto.manitto.SpringTestSupport
+import org.frienitto.manitto.controller.v1.RoomController
+import org.frienitto.manitto.exception.NonAuthorizationException
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.beans.factory.annotation.Autowired
 
-class TokenAuthAopTest : SpringWebMvcTestSupport() {
+class TokenAuthAopTest : SpringTestSupport() {
+
+    companion object {
+        const val TOKEN = "INVALID-TOKEN-99"
+    }
+
+    @Autowired
+    lateinit var roomController: RoomController
 
     @Test
     fun `유효 하지 않은 토큰으로 api를 요청 했을 때`() {
-        mockMvc.perform(get("/api/v1/room/1").header("X-Authorization", "invalid"))
-                .andDo { print() }
-                .andExpect { status().is4xxClientError }
+        assertThrows(NonAuthorizationException::class.java) { roomController.getRoomList(TOKEN) }
     }
 }
