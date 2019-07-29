36e4be24ab202cc922eb6fd8e269f064d6d3cde8
LenKIM <joenggyu0@gmail.com>
2019-07-30 00:58:42 +0900

    fix: update pagable when request roomlist

--- manitto/src/main/kotlin/org/frienitto/manitto/repository/RoomRepository.kt
+++ manitto/src/main/kotlin/org/frienitto/manitto/repository/RoomRepository.kt
@@ -1,9 +1,13 @@
 package org.frienitto.manitto.repository
 
 import org.frienitto.manitto.domain.Room
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface RoomRepository : JpaRepository<Room, Long?> {
+
+    fun findAll(page: PageRequest): Page<Room>
 
     fun findByTitle(title: String): Room?
 }
