486a77d308be32390fbc759f33114349d059b5c3
gksxodnd007 <gksxodnd007@gmail.com>
2019-07-31 00:26:47 +0900

    [Feature] swagger 고도화

--- manitto/src/main/kotlin/org/frienitto/manitto/controller/swagger/model/ModelInfo.kt
+++ manitto/src/main/kotlin/org/frienitto/manitto/controller/swagger/model/ModelInfo.kt
@@ -1,2 +1,17 @@
 package org.frienitto.manitto.controller.swagger.model
 
+import org.frienitto.manitto.dto.*
+
+class RoomDetailInfo : Response<RoomDto>()
+
+class RoomListInfo: Response<List<RoomDto>>()
+
+class VerifyCodeInfo : Response<RegisterToken>()
+
+class SignUpInfo : Response<UserDto>()
+
+class SignInInfo : Response<AccessToken>()
+
+class MatchResultInfo : Response<MatchResultDto>()
+
+class MatchListInfo : Response<List<MissionDto>>()
--- manitto/src/main/kotlin/org/frienitto/manitto/controller/v1/MatchingController.kt
+++ manitto/src/main/kotlin/org/frienitto/manitto/controller/v1/MatchingController.kt
@@ -4,6 +4,8 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
+import org.frienitto.manitto.controller.swagger.model.MatchListInfo
+import org.frienitto.manitto.controller.swagger.model.MatchResultInfo
 import org.frienitto.manitto.dto.MatchRequest
 import org.frienitto.manitto.dto.MatchResultDto
 import org.frienitto.manitto.dto.MissionDto
@@ -21,15 +23,15 @@ import org.springframework.web.bind.annotation.*
 @Api(value = "마니또 매칭 Controller", description = "매칭이 완료된 방의 매칭 상세 정보 API")
 class MatchingController(private val userRoomMapService: UserRoomMapService, private val missionService: MissionService) {
 
-    @ApiOperation(value = "매칭 시작 API")
-    @ApiResponses(value = [ApiResponse(code = 200, message = "Successfully match people")])
+    @ApiOperation(value = "매칭 시작 API", response = MatchResultInfo::class)
+    @ApiResponses(value = [ApiResponse(code = 200, message = "OK")])
     @PostMapping("/matching")
     fun match(@RequestHeader(name = "X-Authorization") token: String, @RequestBody body: MatchRequest): Response<MatchResultDto> {
         return Response(HttpStatus.OK.value(), HttpStatus.OK.reasonPhrase, userRoomMapService.match(body))
     }
 
-    @ApiOperation(value = "매칭 정보 가져오는 API")
-    @ApiResponses(value = [ApiResponse(code = 200, message = "Successfully get matching info")])
+    @ApiOperation(value = "매칭 정보 가져오는 API", response = MatchListInfo::class)
+    @ApiResponses(value = [ApiResponse(code = 200, message = "OK")])
     @GetMapping("/matching-info/room/{id}")
     fun getUserMatchingInfo(@RequestHeader(name = "X-Authorization") token: String, @PathVariable(name = "id") roomId: Long): Response<List<MissionDto>> {
         val result = missionService.getUserMissionsByRoomId(roomId)
